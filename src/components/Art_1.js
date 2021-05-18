c9420a61ed907ad3ef231a54d406e852101c1638
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2021-05-18 22:46:20 +0100

    adding some things and art pics

--- src/components/Art_1.js
+++ src/components/Art_1.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './Art_1.css';
 
+import crazy_clock from "../images/crazy_clock.jpg";
+
 export default class Art_1 extends Component{
     render(){
         return(
             <div className="art_1-container">
                 {window.scrollTo(0, 0)}
                 <h1 className="page-title">Art_1</h1>
+                <h1 className="art-title">Picture 1</h1>
+                <img className="art-image" src={crazy_clock}/>
             </div>
         )
     }
