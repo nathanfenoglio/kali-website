a88e7c135e7cc9f8b2badb1c3e0f18f7e26ff9de
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2021-05-20 14:44:19 +0100

    just adding another pic

--- src/components/Art_1.js
+++ src/components/Art_1.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Art_1.css';
 
 import crazy_clock from "../images/crazy_clock.jpg";
+import abstract_1 from "../images/abstract_1.jpg";
 
 export default class Art_1 extends Component{
     render(){
@@ -11,6 +12,9 @@ export default class Art_1 extends Component{
                 <h1 className="page-title">Art_1</h1>
                 <h1 className="art-title">Picture 1</h1>
                 <img className="art-image" src={crazy_clock}/>
+
+                <h1 className="art-title">Picture 2</h1>
+                <img className="art-image" src={abstract_1}/>
             </div>
         )
     }
