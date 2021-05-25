5dc905686c479c504ea77788b7f3a5248e76e507
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2021-05-25 16:01:52 +0100

    adding grid layout to Art_1 and some more pics

--- src/components/Art_1.js
+++ src/components/Art_1.js
@@ -4,17 +4,25 @@ import './Art_1.css';
 import crazy_clock from "../images/crazy_clock.jpg";
 import abstract_1 from "../images/abstract_1.jpg";
 
+import abstract_2 from "../images/abstract_2.jpg";
+import abstract_collage_1 from "../images/abstract_collage_1.jpg";
+
 export default class Art_1 extends Component{
     render(){
         return(
             <div className="art_1-container">
                 {window.scrollTo(0, 0)}
-                <h1 className="page-title">Art_1</h1>
-                <h1 className="art-title">Picture 1</h1>
+                
                 <img className="art-image" src={crazy_clock}/>
-
-                <h1 className="art-title">Picture 2</h1>
                 <img className="art-image" src={abstract_1}/>
+                <h1 className="art-title">Picture 1</h1>
+                <h1 className="art-title">Picture 2</h1>
+
+                <img className="art-image" src={abstract_2}/>
+                <img className="art-image" src={abstract_collage_1}/>
+                <h1 className="art-title">Picture 3</h1>
+                <h1 className="art-title">Picture 4</h1>
+
             </div>
         )
     }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,7 +13,7 @@ class Navbar extends Component{
     render(){
         return(
             <nav className="NavbarItems">
-                <h1 style={{fontSize:"4vw"}} className="navbar-logo">Kali Esmeralda Fenoglio</h1>
+                <h1 style={{fontSize:"4vw"}} className="navbar-logo">Kali Fenoglio</h1>
                 <div className="menu-icon" onClick={this.handleClick}>
                     <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
                 </div>
